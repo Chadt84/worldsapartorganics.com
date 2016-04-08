893de55a2651dee0b6449ab49a9ce41e5d2ca904
Alejandro Chaves <alejandrochvs32@gmail.com>
2016-04-08 12:19:14 -0500

    Added better gallery

--- js/site.js
+++ js/site.js
@@ -59,3 +59,30 @@ $('ul > li > a').on('click',function(){
         $('#navbar-collapse-btn').click();
     }
 });
+
+$(document).ready(function() {
+	$('.zoom-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			titleSrc: function(item) {
+				return item.el.attr('title');
+			}
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+
+	});
+});
