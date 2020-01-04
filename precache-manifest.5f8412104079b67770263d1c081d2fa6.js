7912ab2ed2a69eb96e6af122694a651adc8d3521
Alejandro Chaves <alejandrochvs@hotmail.com>
2020-01-04 18:40:41 -0500

    Updates

---
+++ precache-manifest.5f8412104079b67770263d1c081d2fa6.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a8522911d280e46c7816edb4edf7a71",
+    "url": "/worldsapartorganics.com/index.html"
+  },
+  {
+    "revision": "b6c1d4a37402556a0887",
+    "url": "/worldsapartorganics.com/static/css/main.bf98273b.chunk.css"
+  },
+  {
+    "revision": "db59574ae03380c73844",
+    "url": "/worldsapartorganics.com/static/js/2.50c65585.chunk.js"
+  },
+  {
+    "revision": "3f8bd776f3f09d63832f1ce796a7c0f1",
+    "url": "/worldsapartorganics.com/static/js/2.50c65585.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b6c1d4a37402556a0887",
+    "url": "/worldsapartorganics.com/static/js/main.45cc4824.chunk.js"
+  },
+  {
+    "revision": "b9217d31f013583ebecd",
+    "url": "/worldsapartorganics.com/static/js/runtime-main.6ac04dff.js"
+  },
+  {
+    "revision": "253b4b8cd7fd8c3cffb3d443ca10c21b",
+    "url": "/worldsapartorganics.com/static/media/1.253b4b8c.png"
+  },
+  {
+    "revision": "377f1ddf69168ad5c679d85678faf923",
+    "url": "/worldsapartorganics.com/static/media/1.377f1ddf.jpg"
+  },
+  {
+    "revision": "477a45e24dbb644ab62b8cb31365f179",
+    "url": "/worldsapartorganics.com/static/media/1.477a45e2.png"
+  },
+  {
+    "revision": "6e5a7a00d7f6fe1d9ac1555fd4530504",
+    "url": "/worldsapartorganics.com/static/media/1.6e5a7a00.png"
+  },
+  {
+    "revision": "ee5b73df1069aeb71b5578033950b279",
+    "url": "/worldsapartorganics.com/static/media/1.ee5b73df.png"
+  },
+  {
+    "revision": "59d6694aae1d3abe6b68c7b63e79590d",
+    "url": "/worldsapartorganics.com/static/media/10.59d6694a.jpg"
+  },
+  {
+    "revision": "e94adeb9c57271acaa14f138f68a5aba",
+    "url": "/worldsapartorganics.com/static/media/10.e94adeb9.png"
+  },
+  {
+    "revision": "020215a1029f8536749411db56ad3d94",
+    "url": "/worldsapartorganics.com/static/media/11.020215a1.png"
+  },
+  {
+    "revision": "5379d94d1373a04eb9421bfb6b469d2b",
+    "url": "/worldsapartorganics.com/static/media/11.5379d94d.png"
+  },
+  {
+    "revision": "e82c21db15674d5c070942e748306745",
+    "url": "/worldsapartorganics.com/static/media/11.e82c21db.jpg"
+  },
+  {
+    "revision": "b1977fb54b8b6dd1d85a140f67f1c689",
+    "url": "/worldsapartorganics.com/static/media/13.b1977fb5.png"
+  },
+  {
+    "revision": "68dcdbfe5024bd08e520ecdea3444eee",
+    "url": "/worldsapartorganics.com/static/media/14.68dcdbfe.png"
+  },
+  {
+    "revision": "4f290a0e3ba731955f9fd1982e11937d",
+    "url": "/worldsapartorganics.com/static/media/15.4f290a0e.png"
+  },
+  {
+    "revision": "0b731d702082708d36121ad601fcd45d",
+    "url": "/worldsapartorganics.com/static/media/2.0b731d70.png"
+  },
+  {
+    "revision": "3547e866c25ccfa151f7116f09ec3c81",
+    "url": "/worldsapartorganics.com/static/media/2.3547e866.png"
+  },
+  {
+    "revision": "b3cc0e8d7c2063a48cbdc921d95b1048",
+    "url": "/worldsapartorganics.com/static/media/2.b3cc0e8d.png"
+  },
+  {
+    "revision": "bddefab80305d05a9ae6d6507121358d",
+    "url": "/worldsapartorganics.com/static/media/2.bddefab8.jpg"
+  },
+  {
+    "revision": "e1f63c99dd3707d9b0bf69b391bd1977",
+    "url": "/worldsapartorganics.com/static/media/2.e1f63c99.jpg"
+  },
+  {
+    "revision": "2fd9e418c3a68fd69dcbce665a422b65",
+    "url": "/worldsapartorganics.com/static/media/3.2fd9e418.png"
+  },
+  {
+    "revision": "5ca3c073e143b20e4180c9bdb680e50f",
+    "url": "/worldsapartorganics.com/static/media/3.5ca3c073.png"
+  },
+  {
+    "revision": "bee7d0b35048802734bc63d43ff0b144",
+    "url": "/worldsapartorganics.com/static/media/3.bee7d0b3.png"
+  },
+  {
+    "revision": "f07e719d707270fd3efbe533d901a330",
+    "url": "/worldsapartorganics.com/static/media/3.f07e719d.jpg"
+  },
+  {
+    "revision": "37a888bf5e1d7a1ed5f4e6df7e3b22e1",
+    "url": "/worldsapartorganics.com/static/media/4.37a888bf.png"
+  },
+  {
+    "revision": "3d4f92b83e66f4d4ee7c20a52b43805e",
+    "url": "/worldsapartorganics.com/static/media/4.3d4f92b8.png"
+  },
+  {
+    "revision": "6e1f3b732102092fc17f665d219ae29d",
+    "url": "/worldsapartorganics.com/static/media/4.6e1f3b73.png"
+  },
+  {
+    "revision": "7bb67b8339d609c38fd20e7cda7c5a01",
+    "url": "/worldsapartorganics.com/static/media/4.7bb67b83.png"
+  },
+  {
+    "revision": "e87248f41f334abfb4440d3ce542bcb7",
+    "url": "/worldsapartorganics.com/static/media/4.e87248f4.jpg"
+  },
+  {
+    "revision": "2dc873e5d577f2ce96be0fbb2788f80b",
+    "url": "/worldsapartorganics.com/static/media/5.2dc873e5.png"
+  },
+  {
+    "revision": "336f1a2f476433db6d2144d5f46fcdbe",
+    "url": "/worldsapartorganics.com/static/media/5.336f1a2f.jpg"
+  },
+  {
+    "revision": "4ddfe4c0d3f8263fc1b10b108b37564e",
+    "url": "/worldsapartorganics.com/static/media/5.4ddfe4c0.png"
+  },
+  {
+    "revision": "4fc2c8ee25134055131acd574573f0a4",
+    "url": "/worldsapartorganics.com/static/media/5.4fc2c8ee.jpeg"
+  },
+  {
+    "revision": "012d31de9743974992b6729b1e45b1ba",
+    "url": "/worldsapartorganics.com/static/media/6.012d31de.png"
+  },
+  {
+    "revision": "1ffdf4d6b601c627658673b4d2415aba",
+    "url": "/worldsapartorganics.com/static/media/6.1ffdf4d6.png"
+  },
+  {
+    "revision": "3c51518f5700ce24bb877abccca5828f",
+    "url": "/worldsapartorganics.com/static/media/6.3c51518f.jpg"
+  },
+  {
+    "revision": "46b810e4727fce98e81465989c273dfd",
+    "url": "/worldsapartorganics.com/static/media/6.46b810e4.png"
+  },
+  {
+    "revision": "6d6417586fb405174b6c10c7a00964df",
+    "url": "/worldsapartorganics.com/static/media/6.6d641758.jpg"
+  },
+  {
+    "revision": "a68dd9c76416cdc09111298f42251319",
+    "url": "/worldsapartorganics.com/static/media/6.a68dd9c7.png"
+  },
+  {
+    "revision": "f942f46e560be2fe8d29c38739d39599",
+    "url": "/worldsapartorganics.com/static/media/6.f942f46e.png"
+  },
+  {
+    "revision": "06915b0d8c4e7b0c7fb1c83cd48d44a4",
+    "url": "/worldsapartorganics.com/static/media/7.06915b0d.png"
+  },
+  {
+    "revision": "2fa12d114d99e3f38aa289ed811f4490",
+    "url": "/worldsapartorganics.com/static/media/7.2fa12d11.jpg"
+  },
+  {
+    "revision": "584075aab9e36bfd6c59b864da74363e",
+    "url": "/worldsapartorganics.com/static/media/7.584075aa.png"
+  },
+  {
+    "revision": "2683e4c90e6ebf53a4b39e8173f78da9",
+    "url": "/worldsapartorganics.com/static/media/8.2683e4c9.png"
+  },
+  {
+    "revision": "662c806a5e07d807398bfae61698263e",
+    "url": "/worldsapartorganics.com/static/media/8.662c806a.png"
+  },
+  {
+    "revision": "ba0f9c985a91c1ae2c5c468454ed81e1",
+    "url": "/worldsapartorganics.com/static/media/8.ba0f9c98.jpg"
+  },
+  {
+    "revision": "c3fe2ca57cdf9691994da9ab571287cc",
+    "url": "/worldsapartorganics.com/static/media/8.c3fe2ca5.png"
+  },
+  {
+    "revision": "6b4bfc19e904b6fdb291513cb4fa330b",
+    "url": "/worldsapartorganics.com/static/media/9.6b4bfc19.jpg"
+  },
+  {
+    "revision": "a738d02550b1c8def92b43a48fb5d148",
+    "url": "/worldsapartorganics.com/static/media/9.a738d025.png"
+  },
+  {
+    "revision": "18bd26348b5b782ce9682c16a9bf4f95",
+    "url": "/worldsapartorganics.com/static/media/Worlds apart organics.18bd2634.png"
+  },
+  {
+    "revision": "6f196ad5b7fb0d92960557a01cd3b494",
+    "url": "/worldsapartorganics.com/static/media/bg3.6f196ad5.png"
+  },
+  {
+    "revision": "e8b842e42ba6e578a35ece6e5b60770a",
+    "url": "/worldsapartorganics.com/static/media/header.e8b842e4.png"
+  }
+]);
